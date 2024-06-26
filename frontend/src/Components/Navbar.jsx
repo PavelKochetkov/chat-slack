25ac12bc12b24f8bebf499c55fca650438a856f9
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-27 00:13:10 +0300

    creating an input form

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-white">
     <div className="container">
-      <a className="navbar-brand" href="/">Chat (Slack)</a>
+      <a className="navbar-brand" href="/">Chat Slack</a>
     </div>
   </nav>
 );
