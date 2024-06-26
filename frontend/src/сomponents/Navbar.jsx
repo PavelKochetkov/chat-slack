aa3dedc0be7e15d124feb3576d2f7557b9610713
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-26 21:48:03 +0300

    routing setup

---
+++ frontend/src/сomponents/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-white">
+    <div className="container">
+      <a className="navbar-brand" href="/">Chat (Slack)</a>
+    </div>
+  </nav>
+);
+
+export default Navbar;
