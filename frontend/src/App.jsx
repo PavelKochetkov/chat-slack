e4652d041276c1dd8f949f1f2b7bfec1f8a47b8d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-26 23:46:08 +0300

    creating an input form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
-import Login from './pages/Login';
+import Login from './pages/Login.jsx';
 
 const App = () => (
   <>
