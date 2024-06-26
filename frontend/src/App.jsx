5ea7c6deceb6fce931c8e8e180a06b97244c275d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-26 23:33:37 +0300

    creating an input form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar.jsx';
-import Login from './Pages/Login.jsx';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
 
 const App = () => (
   <>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../Components/LoginForm.jsx';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => (
   <div className="container-fluid h-100">
