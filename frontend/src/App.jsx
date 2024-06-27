afbce4ebb02c737def146f348e3625b2496222ba
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-27 22:03:11 +0300

    fix styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,14 @@ import Login from './Page/Login.jsx';
 import NotFoundPage from './Page/NotFoundPage.jsx';
 
 const App = () => (
-  <>
+  <div className="h-100 bg-light">
     <Navbar />
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
-  </>
+  </div>
 );
 
 export default App;
