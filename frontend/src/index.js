ce9d5a8a9bc1f21e323bd768e7488c6a6e2715a4
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-28 19:59:40 +0300

    fix styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const chat = ReactDOM.createRoot(document.getElementById('chat'));
 chat.render(
