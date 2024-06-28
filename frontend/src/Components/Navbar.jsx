d2165cc37235d4bc8a4484efd462deb05c350cf6
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-28 19:55:06 +0300

    fix styles form and navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-white">
+  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
     <div className="container">
       <a className="navbar-brand" href="/">Chat Slack</a>
     </div>
