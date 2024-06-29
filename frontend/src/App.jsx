f7cbe44ccded2814f8877add168faba13388d2d6
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-06-29 16:46:18 +0300

    implementation of user authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx';
 import Login from './Page/Login.jsx';
 import NotFoundPage from './Page/NotFoundPage.jsx';
 
-const App = () => (
-  <div className="d-flex flex-column h-100">
-    <Navbar />
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  return (
+    <div className="d-flex flex-column h-100">
+      <Navbar />
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,51 +1,63 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
 import axios from 'axios';
 
-const LoginForm = () => (
-  <Formik
-    initialValues={{ username: '', password: '' }}
-    onSubmit={async (values) => {
-      const userData = await axios.post('/api/auth/login', values);
-      console.log(userData.data);
-    }}
-  >
-    {({ isSubmitting }) => (
-      <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-        <h1 className="text-center mb-4">Войти</h1>
-        <div className="form-floating mb-3">
-          <Field
-            type="text"
-            name="username"
-            className="form-control"
-            placeholder="Ваш ник"
-            id="username"
-            required
-            autoFocus
-          />
-          <label htmlFor="username">Ваш ник</label>
-        </div>
-        <div className="form-floating mb-4">
-          <Field
-            type="password"
-            name="password"
-            className="form-control"
-            placeholder="Пароль"
-            id="password"
-            required
-          />
-          <label className="form-label" htmlFor="password">Пароль</label>
-        </div>
-        <button
-          type="submit"
-          className="w-100 mb-3 btn btn-outline-primary"
-          disabled={isSubmitting}
-        >
-          Войти
-        </button>
-      </Form>
-    )}
-  </Formik>
-);
+const LoginForm = () => {
+  const [loginError, setLoginError] = useState('');
+  const navigate = useNavigate();
+
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          const responce = await axios.post('/api/v1/login', values);
+          localStorage.setItem('token', responce.data.token);
+          navigate('/');
+        } catch (error) {
+          setLoginError('Ошибка авторизации. Проверьте введенные данные');
+          setSubmitting(false);
+        }
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="col-12 col-md-6 mt-3 mt-mb-0">
+          <h1 className="text-center mb-4">Войти</h1>
+          {loginError && <div className="alert alert-danger">{loginError}</div>}
+          <div className="form-floating mb-3">
+            <Field
+              type="text"
+              name="username"
+              className="form-control"
+              placeholder="Ваш ник"
+              id="username"
+              required
+              autoFocus
+            />
+            <label htmlFor="username">Ваш ник</label>
+          </div>
+          <div className="form-floating mb-4">
+            <Field
+              type="password"
+              name="password"
+              className="form-control"
+              placeholder="Пароль"
+              id="password"
+              required
+            />
+            <label className="form-label" htmlFor="password">Пароль</label>
+          </div>
+          <button
+            type="submit"
+            className="w-100 mb-3 btn btn-outline-primary"
+            disabled={isSubmitting}
+          >
+            Войти
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 export default LoginForm;
