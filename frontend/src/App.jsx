421192f7f9129451f8c791c10b9be8ba82629662
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-07-14 20:36:51 +0300

    create PrivateRout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
-  Route, Routes, useNavigate,
+  Route, Routes,
 } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider, ErrorBoundary } from '@rollbar/react';
@@ -9,38 +9,28 @@ import Login from './Page/Login.jsx';
 import ChatPage from './Page/ChatPage.jsx';
 import Signup from './Page/Signup.jsx';
 import NotFoundPage from './Page/NotFoundPage.jsx';
+import PrivateRoute from './Components/PrivateRoute.jsx';
 
 const rollbarConfig = {
   accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
   environment: 'production',
 };
 
-const App = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      navigate('/login');
-    }
-  }, [navigate]);
-
-  return (
-    <div className="d-flex flex-column h-100">
-      <Provider config={rollbarConfig}>
-        <ErrorBoundary>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<ChatPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-          <ToastContainer />
-        </ErrorBoundary>
-      </Provider>
-    </div>
-  );
-};
+const App = () => (
+  <div className="d-flex flex-column h-100">
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PrivateRoute element={ChatPage} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <ToastContainer />
+      </ErrorBoundary>
+    </Provider>
+  </div>
+);
 
 export default App;
