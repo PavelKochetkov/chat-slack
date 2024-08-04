6bc286707d7d6cee99fb49345ca5ab0baa538f2d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-08-04 16:23:38 +0300

    Added validation of spaces. Now the channel name cannot consist only of spaces

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -23,6 +23,7 @@ const ModalContainer = () => {
   const currentChannelId = useSelector((state) => state.app.currentChannelId);
   const addChannelSchema = Yup.object().shape({
     name: Yup.string()
+      .trim()
       .min(3, t('errors.range'))
       .max(20, t('errors.range'))
       .required(t('errors.required'))
