9680d4ff486ebaa5c48b3e6ef50e937a8515c1d1
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-08-05 22:53:13 +0300

    Added scrolling when receiving new messages and switching between channels.

--- frontend/src/components/MessageBox.jsx
+++ frontend/src/components/MessageBox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { useGetMessagesQuery } from '../api/chatApi.js';
 import socket from '../socket.js';
@@ -8,6 +8,13 @@ const MessageBox = () => {
   const { data: messages, isLoading, refetch } = useGetMessagesQuery();
   const currentChannelId = useSelector((state) => state.app.currentChannelId);
   const filtredMessages = messages?.filter((message) => message.channelId === currentChannelId);
+  const messageRef = useRef(null);
+
+  useEffect(() => {
+    if (messageRef.current) {
+      messageRef.current.scrollTop = messageRef.current.scrollHeight;
+    }
+  }, [messages.length, currentChannelId]);
 
   useEffect(() => {
     const handleNewMessage = async () => {
@@ -19,10 +26,10 @@ const MessageBox = () => {
     return () => {
       socket.off('newMessage');
     };
-  }, [refetch]);
+  }, [refetch, messageRef]);
 
   return (
-    <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+    <div id="messages-box" className="chat-messages overflow-auto px-5" ref={messageRef}>
       {isLoading && <Loading />}
       {filtredMessages?.map((message) => (
         <div className="text-break mb-2" key={message.id}>
