7740200c37e0c49368cbf376b70d530438f4b0f4
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-08-12 20:29:39 +0300

    moved the header preparation function into the api helper

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,16 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import prepareHeaders from '../utils/apiHelpers';
 
 const chatApi = createApi({
   reducerPath: 'chatApi',
   baseQuery: fetchBaseQuery({
     baseUrl: '/api/v1',
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
+    prepareHeaders: (headers) => prepareHeaders(headers),
   }),
   tagTypes: ['Channels', 'Messages'],
   endpoints: (builder) => ({
