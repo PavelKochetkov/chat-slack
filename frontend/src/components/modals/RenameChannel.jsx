f9908c5d5dc1a8880eae9cfc5a0384c4493b1ebb
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-08-30 22:58:33 +0300

    check input focus

--- frontend/src/components/modals/RenameChannel.jsx
+++ frontend/src/components/modals/RenameChannel.jsx
@@ -85,6 +85,7 @@ const RenameChannel = (props) => {
                 ref={inputRef}
                 className={`form-control ${!isValid ? 'mb-2 is-invalid' : 'mb-2'}`}
                 id="name"
+                autoFocus
               />
 
               <Form.Label className="visually-hidden" htmlFor="name">{t('modal.label')}</Form.Label>
