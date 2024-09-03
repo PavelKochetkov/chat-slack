8ac01c8fa0c54c89eb025a2fd626f8e05e55124d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-09-03 22:24:23 +0300

    Create a selector for current messages

--- frontend/src/api/messagesApi.js
+++ frontend/src/api/messagesApi.js
@@ -1,5 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createSelector } from '@reduxjs/toolkit';
 import prepareHeaders from '../utils/apiHelpers';
+// eslint-disable-next-line import/no-cycle
+import { selectCurrentChannelId } from '../store/slice/appSlice';
 import { generateAuthApiRoute } from '../utils/routes';
 
 const messagesApi = createApi({
@@ -23,7 +26,23 @@ const messagesApi = createApi({
     }),
   }),
 });
+
+const selectMessages = messagesApi.endpoints.getMessages.select();
+
+const selectMessagesData = createSelector(
+  selectMessages,
+  (messagesState) => messagesState.data ?? [],
+);
+
+export const selectCurrentMessages = createSelector(
+  [selectMessagesData, selectCurrentChannelId],
+  (messages, currentChannelId) => (
+    messages.filter(({ channelId }) => channelId === currentChannelId) || null
+  ),
+);
+
 export default messagesApi;
+
 export const {
   useGetMessagesQuery,
   useAddMessageMutation,
