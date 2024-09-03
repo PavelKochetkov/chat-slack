bb06bad593aa4ad03806408051e6f3346cdfec17
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-09-04 00:01:47 +0300

    renamed the component

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HeaderMessageContainer from './HeaderMessageContainer';
+import Header from './Header';
 import MessageBox from './MessageBox';
 import MessageForm from './MessageForm';
 
 const MessageContainer = () => (
   <div className="col p-0 h-100">
     <div className="d-flex flex-column h-100">
-      <HeaderMessageContainer />
+      <Header />
       <MessageBox />
       <MessageForm />
     </div>
