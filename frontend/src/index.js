bf3accaab64f68faa97b0bea8e75ff0263de4619
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-09-03 10:26:20 +0300

    Got a socket instance in index.js.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { io } from 'socket.io-client';
 import { BrowserRouter } from 'react-router-dom';
-import socket from './socket.js';
 import init from './init.js';
 
 const run = async () => {
   const root = ReactDOM.createRoot(document.getElementById('chat'));
+  const socket = io();
   const app = await init(socket);
   root.render(
     <React.StrictMode>
